88dfe69c9f3b3c1f5c40dddf4c98e002f0b4572c
Anatoliy Shikin <t7313@ya.ru>
2025-03-18 15:49:55 +0300

    added @Repository

--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -2,6 +2,7 @@ package org.example.repository;
 
 import org.example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
@@ -9,6 +10,7 @@ import java.util.Optional;
 /**
  * @author Anatoliy Shikin
  */
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByLastNameIgnoreCase(String lastName);
 }
